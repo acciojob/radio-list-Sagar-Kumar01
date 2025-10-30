e7af3c0671fc739535521ed412b47890dba7bfe7
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-10-30 16:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// for(let i = 1; i<=6; i++){
+// 	if(i == 3){
+// 		let labal = document.createElement('label');
+// 		let labal = document.createElement('input');
+// 	}else if(i == 2){
+		
+// 	}else if((i%2) == 0){
+		
+// 	}else{
+		
+// 	}
+// }
